efb3d9fd3eab42877dd204a3b3511008e469f0fd
ChrisC <chrischan8551@gmail.com>
2023-02-15 21:05:52 -0600

    favicon

---
+++ tasks.js
@@ -0,0 +1,44 @@
+// - [] db schema
+// - [] read me
+// - [] album route - create
+// - [] album route - get
+// - [] album route - edit
+// - [] album route - delete
+// - [] photo route - create
+// - [] photo route - get
+// - [] photo route - edit
+// - [] photo route - delete
+// - [] user route - create
+// - [] user route - get
+// - [] user route - edit
+// - [] user route - delete
+// - [] seeds photos
+// - [] seeds albums
+
+
+// - [] home page
+// - [] navigation
+// - [] profile
+// - [] photos
+
+// - [] AlbumsPage
+// - [] AlbumsDetailPage
+// - [] EditAlbumForm
+// - [] PhotoPage
+// - [] PhotoDetailPage
+// - [] EditPhotoForm
+// - [] SignupFormPage
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
+// - []
