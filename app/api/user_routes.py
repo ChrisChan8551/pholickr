184acd700afe19c02d0dc0fe0e5c30c0f9d9e422
ChrisC <chrischan8551@gmail.com>
2023-02-16 19:10:04 -0600

    GET USER

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import User
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
+
 
 def validation_errors_to_error_messages(validation_errors):
     """
@@ -14,21 +15,42 @@ def validation_errors_to_error_messages(validation_errors):
             errorMessages.append(f'{field} : {error}')
     return errorMessages
 
+
 @user_routes.route('/')
-@login_required
+# @login_required
 def users():
-    """
-    Query for all users and returns them in a list of user dictionaries
-    """
+    print('************ GET ALL USERS ****************')
+
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
 
 
 @user_routes.route('/<int:id>')
-@login_required
+# @login_required
 def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
+
+    print('**************** GET 1 USER ****************')
+
     user = User.query.get(id)
     return user.to_dict()
+
+# @user_routes.route('/<int:id>', methods=['PUT'])
+# # @login_required
+# def edit_user(id):
+#     # print('*********************EDIT User*******************************')
+#     # form = ProfileForm()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+
+#     if form.validate_on_submit():
+
+#         data = form.data
+#         # print(data, 'helloo from backend')
+
+#         user= User.query.get(id)
+#         for key, value in data.items():
+#             setattr(user, key, value)
+#         # print('*********************UPDATED User*******************************')
+#         db.session.commit()
+
+#         return user.to_dict_with_related()
+#     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
