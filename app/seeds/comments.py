3f267dba187118c0cf7fcda04a7c9e0258ff2e7c
ChrisC <chrischan8551@gmail.com>
2023-08-09 16:23:00 -0500

    photocount

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -4,7 +4,7 @@ import random
 
 def seed_comments():
     user_count = 13
-    photo_count = 10
+    photo_count = 935
     commentData = []
     for photoId in range(1, photo_count):
         for _ in range(5):
