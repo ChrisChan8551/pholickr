bee9a5d2a61f823a6115e69986deca33ea32129a
chrischan8551 <chrischan8551@gmail.com>
2024-04-25 01:08:30 -0500

    socket

--- app/socket.py
+++ app/socket.py
@@ -4,14 +4,13 @@ import os
 # Configure CORS origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
-        'http://pholickr.onrender.com/',
-        'https://pholickr.onrender.com/'
+        'http://pholickr.onrender.com',
+        'https://pholickr.onrender.com'
     ]
 else:
     origins = "*"
 
-
-socketio = SocketIO(cors_allowed_origins=origins)
+socketio = SocketIO(app, cors_allowed_origins=origins)
 
 
 @socketio.on("chat")
