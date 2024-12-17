048298257c65c1a90397baa8aeeb5faf98227dd1
ChrisChan8551 <chrischan8551@gmail.com>
2024-12-17 00:17:24 -0600

    picture titles

--- react-app/src/components/GridLayout/index.js
+++ react-app/src/components/GridLayout/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import PopOver from '../PopOver';
 import './GridLayout.css';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function GridLayout({
 	items = [],
 	buttonLabel = 'Options',
 	onItemClick = null,
 	renderItemActions = null,
+
 }) {
 	return (
 		<div className='GridLayout--Container'>
@@ -26,63 +29,65 @@ function GridLayout({
 export default GridLayout;
 
 function GridItem({ item, buttonLabel, onItemClick, renderItemActions }) {
-	const [popOverOpen, setPopOverOpen] = useState(false);
-	const { image, imageUrl = image } = item;
+    const [popOverOpen, setPopOverOpen] = useState(false);
+    const { image, imageUrl = image } = item;
+    const currentUser = useSelector((state) => state.session.user);
+    
+    return (
+      <div
+        className='GridLayout--Item'
+        onClick={
+          typeof onItemClick !== 'function' ? undefined : (event) => onItemClick(item, event)
+        }
+        style={{
+          cursor: onItemClick ? 'pointer' : 'auto',
+        }}
+      >
+        <div className='GridLayout--ImageContainer'>
+          <img
+            src={imageUrl}
+            crossOrigin='anonymous'
+            className='GridLayout--Image'
+            alt={item.title}
+          />
 
-	return (
-		<div
-			className='GridLayout--Item'
-			onClick={
-				typeof onItemClick !== 'function'
-					? undefined
-					: (event) => onItemClick(item, event)
-			}
-			style={{
-				cursor: onItemClick ? 'pointer' : 'auto',
-			}}
-		>
-			<img
-				src={imageUrl}
-				crossOrigin='anonymous'
-				className='GridLayout--Image'
-				alt=''
-			/>
-			{!!renderItemActions && (
-				<div className='GridLayout--Actions'>
-					<PopOver
-						open={popOverOpen}
-						setOpen={setPopOverOpen}
-						style={{
-							top: '25px',
-						}}
-						button={
-							<button
-								className='blue-button'
-								onClick={(event) => {
-									event.preventDefault();
-									event.stopPropagation();
-									setPopOverOpen(true);
-								}}
-							>
-								{buttonLabel}
-							</button>
-						}
-					>
-						<div
-							className='GridLayout--Action--Content'
-							onClick={(event) => {
-								event.preventDefault();
-								event.stopPropagation();
-							}}
-						>
-							{typeof renderItemActions === 'function' &&
-								renderItemActions(item, () =>
-									setPopOverOpen(false)
-								)}
-						</div>
-					</PopOver>
-				</div>
-			)}
-		</div>
-	);
-}
+          {currentUser && <div className='GridLayout--Title'>{item.title}</div>}
+        </div>
+
+        {!!renderItemActions && (
+          <div className='GridLayout--Actions'>
+            <PopOver
+              open={popOverOpen}
+              setOpen={setPopOverOpen}
+              style={{
+                top: '25px',
+              }}
+              button={
+                <button
+                  className='blue-button'
+                  onClick={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    setPopOverOpen(true);
+                  }}
+                >
+                  {buttonLabel}
+                </button>
+              }
+            >
+              <div
+                className='GridLayout--Action--Content'
+                onClick={(event) => {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }}
+              >
+                {typeof renderItemActions === 'function' &&
+                  renderItemActions(item, () => setPopOverOpen(false))}
+              </div>
+            </PopOver>
+          </div>
+        )}
+      </div>
+    );
+  }
